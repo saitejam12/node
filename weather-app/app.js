6fead34a235df1c9477f63ea8ce68f895021215a
saitejam12 <saitejamuddana@gmail.com>
2020-06-29 23:15:13 -0500

    Create app.js

---
+++ weather-app/app.js
@@ -0,0 +1,10 @@
+console.log('Weather App');
+console.log('Initializing');
+const fs = require('fs');
+
+//async calling using settimeout
+setTimeout(() => {
+  console.log('timer 2 sec interval');
+}, 2000);
+
+console.log('Terminating');
